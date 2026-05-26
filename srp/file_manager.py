b691ee0ad389f381ad2c596c24a4268ea89c1ec4
KisoKucode <kisokucode@gmail.com>
2026-05-25 19:35:46 -0500

    Aplicar SRP separando compresion de archivos

--- srp/file_manager.py
+++ srp/file_manager.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from zipfile import ZipFile
 
+
 class FileManager:
     def __init__(self, filename):
         self.path = Path(filename)
@@ -11,6 +12,11 @@ class FileManager:
 
     def write(self, data, encoding="utf-8"):
         self.path.write_text(data, encoding)
+
+
+class ZipFileManager:
+    def __init__(self, filename):
+        self.path = Path(filename)
 
     def compress(self):
         with ZipFile(self.path.with_suffix(".zip"), mode="w") as archive:
