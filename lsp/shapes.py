44dbf767c83fd265bb32422c889916bd707e6d12
KisoKucode <kisokucode@gmail.com>
2026-05-25 19:40:14 -0500

    Aplicar LSP con figuras sustituibles

--- lsp/shapes.py
+++ lsp/shapes.py
@@ -1,8 +1,30 @@
-"""Clients should not be forced to depend upon methods that they do not use. Interfaces belong to clients, not to hierarchies."""
-class Rectangle:
+"""Subtypes must be substitutable for their base types without changing correctness."""
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+
+class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def calculate_area(self):
         return self.width * self.height
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def calculate_area(self):
+        return self.side**2
+
+
+def total_area(shapes):
+    return sum(shape.calculate_area() for shape in shapes)
